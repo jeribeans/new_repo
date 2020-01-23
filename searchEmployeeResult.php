81e47ee31b6b1d02e86ba438910d0ed5eebaebc6
Jericho Alexis <jericho.alexis00@gmail.com>
2020-01-23 20:57:32 +0800

    update as of 01-23-2020(9pm)

--- searchEmployeeResult.php
+++ searchEmployeeResult.php
@@ -13,6 +13,15 @@ if (!in_array($_SESSION['department'], array('Admin', 'SuperAdmin', 'AdminNOC', 
   header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/index2.php");
 }
 
+
+
+if(isset($_POST[$submit])){
+    
+}
+
+
+
+
 ?>
 
 
@@ -49,21 +58,7 @@ if(isset($_POST['search'])){
         $id = $row['user_ID'];
         
         //SELECT * FROM timecheck JOIN schedule ON schedule.user_ID = timecheck.user_ID JOIN shift ON shift.shift_ID = schedule.shift_ID WHERE timecheck.user_ID = '$id' AND timecheck.login_date = schedule.sched_Date AND timecheck.login_date BETWEEN '$START' AND '$END';
- 
- ?>
-      <!--   <table class="table table-hover table-striped table-condensed table-bordered" >
-                            <thead>
-                                <tr>
-                                    <th>Shift</th>
-                                    <th>Schedule</th>
-                                    <th>Login Date and Time</th>
-                                    <th>Logout Date and Time</th>
-                                    <th>Schedule Status</th>
-                                </tr>
-                        
-                            </thead>   -->
-
-<?php 
+  
         
         // $searchAttendance = mysqli_query($conn,"SELECT * FROM timecheck JOIN user ON user.user_ID = timecheck.user_ID JOIN schedule ON schedule.schedule_ID = timecheck.user_ID JOIN shift ON shift.shift_ID = schedule.shift_ID WHERE timecheck.user_ID = '$id' AND timecheck.login_date BETWEEN '$START' AND '$END'");
         // $row3 = mysqli_fetch_assoc($searchAttendance);
@@ -96,30 +91,59 @@ if(isset($_POST['search'])){
         <?php
 
         
-
+        ?><form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="DTRForm" method="post"><?php
         for ($i =0; $i < date("d", strtotime($END)); $i++){
 
             $day = $START;
             
-            $searchAttendance = mysqli_query($conn,"SELECT * FROM timecheck JOIN user ON user.user_ID = timecheck.user_ID JOIN schedule ON schedule.schedule_ID = timecheck.user_ID JOIN shift ON shift.shift_ID = schedule.shift_ID WHERE timecheck.user_ID = '$id' AND timecheck.login_date BETWEEN '$day' AND '$day'");
-            $row3 = mysqli_fetch_assoc($searchAttendance);
+
+            //Select * FROM schedule JOIN shift on shift.shift_ID = schedule.shift_ID JOIN user ON user.user_ID = schedule.user_ID where schedule.user_ID = '9' and schedule.sched_Date = '2019-12-01'
+            
+            $searchAttendance = mysqli_query($conn,"SELECT * FROM schedule JOIN shift ON shift.shift_ID = schedule.shift_ID JOIN user ON user.user_ID = schedule.user_ID WHERE schedule.sched_Date = '$day' AND user.user_ID = '$id'");
+
+            $searchTimecheck = mysqli_query($conn,"SELECT * FROM timecheck  WHERE timecheck.login_date = '$day' AND timecheck.user_ID = '$id'");
+
+
+            //$searchAttendance = mysqli_query($conn,"SELECT * FROM timecheck JOIN user ON user.user_ID = timecheck.user_ID JOIN schedule ON schedule.schedule_ID = timecheck.user_ID JOIN shift ON shift.shift_ID = schedule.shift_ID WHERE timecheck.user_ID = '$id' AND timecheck.login_date BETWEEN '$day' AND '$day'");
             
 
-          
+            $row3 = mysqli_fetch_assoc($searchAttendance);
+            $row4 = mysqli_fetch_assoc($searchTimecheck);            
+
+            
 
             // echo "<tr>";
-            echo "<td>".date("d", strtotime($row3['login_date']))."<td>";
-            echo "<td>".date("M", strtotime($row3['login_date']))."<td>";
-            echo "<td>".$row3['shift_time_in']."<td>";
-            echo "<td>".$row3['login_time']."<td>";
-            echo "<td>".$row3['logout_time']."<td>";
+            echo "<td>".date("d", strtotime($row3['sched_Date']))."</td>";
+            echo "<td>".date("M", strtotime($row3['sched_Date']))."</td>";
+            
+            echo "<td>".$row3['shift_time_in']."</td>";
+            echo "<td>".$row3['shift_time_out']."</td>";
+            echo "<td>".$row4['login_time']."</td>";
+            echo "<td>".$row4['logout_time']."</td>";
+            echo "<td><input class='form-control' type='text' name='dtrOT".$i."' placeholder = 'Insert overtime hours (e.g. 2, 3, 4, etc.)' >"."</td>";
+            echo "<td><input class='form-control' type='text' name='dtrNightDiff".$i."' placeholder = 'Insert night differential (e.g. 2, 3, 4, etc.)' >"."</td>";
+            echo "<td><input class='form-control' type='text' name='dtrNightDiff".$i."' placeholder = 'Insert night differential (e.g. 2, 3, 4, etc.)' >"."</td>";
+            echo "<td><input class='form-control' type='text' name='dtrNightDiff".$i."' placeholder = 'Insert night differential (e.g. 2, 3, 4, etc.)' >"."</td>";
+            ?> <td>
+                   <select class="form-control" name="shift">
+                       <option disabled selected>Select Work Day</option>
+                       <option >Regular</option>
+                       <option >Legal Holiday</option>
+                       <option >Special Non-Working</option>
+                    </select>                                                
+                </td>
+            
+            <?php
+
+
             echo "</tr>";
 
             $START = date ("Y-m-d", strtotime("+1 days", strtotime($START)));
                             
 
             }
-
+            ?> <tr><input type="submit" name="submit" class="btn btn-def btn-block" value="Set" /></tr><?php 
+            echo "</form>";
 
 
         
